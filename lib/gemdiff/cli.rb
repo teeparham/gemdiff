5d03b49365a12e1431ba4aad2344fde1fd522299
Tee Parham <tee@neighborland.com>
2014-03-03 10:21:36 -0700

    Improve command descriptions

--- lib/gemdiff/cli.rb
+++ lib/gemdiff/cli.rb
@@ -4,7 +4,7 @@ module Gemdiff
   class CLI < Thor
     default_task :outdated
 
-    desc 'find gem_name', 'Find the github repository URL for a gem'
+    desc 'find <gem>', 'Find the github repository URL for a gem'
     def find(gem_name)
       gem = OutdatedGem.new(gem_name)
       if gem.repo?
@@ -15,25 +15,25 @@ module Gemdiff
       gem
     end
 
-    desc 'open gem_name', 'Open the github repository for a gem'
+    desc 'open <gem>', 'Open the github repository for a gem'
     def open(gem_name)
       gem = find(gem_name)
       gem.open
     end
 
-    desc 'releases gem_name', 'Open the github releases page for a gem'
+    desc 'releases <gem>', 'Open the github releases page for a gem'
     def releases(gem_name)
       gem = find(gem_name)
       gem.releases
     end
 
-    desc 'commits gem_name', 'Open the github master branch commits page for a gem'
+    desc 'commits <gem>', 'Open the github master branch commits page for a gem'
     def commits(gem_name)
       gem = find(gem_name)
       gem.commits
     end
 
-    desc 'compare gem_name', <<DESC
+    desc 'compare <gem> [<versions>...]', <<DESC
 Compare gem versions. Opens the compare view between the specified new and old versions.
 If versions are not specified, your bundle is inspected and the latest version of the
 gem is compared with the current version in your bundle.
