061f5cad27c2053bea401f15a8b8b3ec0aa58bab
Tee Parham <tee@neighborland.com>
2014-03-02 21:40:42 -0700

    Update CLI method descriptions

--- lib/gemdiff/cli.rb
+++ lib/gemdiff/cli.rb
@@ -4,7 +4,7 @@ module Gemdiff
   class CLI < Thor
     default_task :outdated
 
-    desc 'find', 'find github repository URL for a gem'
+    desc 'find', 'Find the github repository URL for a gem'
     def find(gem_name)
       repo = RepoFinder.github_url(gem_name)
       puts "Could not find github repository for #{gem_name}." unless repo
@@ -12,13 +12,13 @@ module Gemdiff
       repo
     end
 
-    desc 'open', 'open github repository for a gem'
+    desc 'open', 'Open the github repository for a gem'
     def open(gem_name)
       repo = find(gem_name)
       `open #{repo}` if repo
     end
 
-    desc 'compare', 'compare gem versions'
+    desc 'compare', 'Compare gem versions. Opens the compare view between the specified new and old versions. If versions are not specified, your bundle is inspected and the latest version of the gem is compared with the current version in your bundle.'
     method_option :new, aliases: '-n', desc: 'new gem version'
     method_option :old, aliases: '-o', desc: 'old gem version'
     #method_option :prompt, aliases: '-p', desc: 'prompt before opening the compare view'
@@ -40,7 +40,8 @@ module Gemdiff
       `open #{repo}/compare/v#{old_version}...v#{new_version}`
     end
 
-    desc 'outdated', 'compare each outdated gem in the bundle'
+    desc 'outdated', 'Compare each outdated gem in the bundle. You will be prompted to open each compare view.'
+    method_option :no_skip, aliases: '-s', desc: 'skip warning about tag format'
     def outdated
       puts "Checking for outdated gems in your bundle..."
       inspector = BundleInspector.new
