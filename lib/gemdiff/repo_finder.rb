aeba886ffb0fb9098c7ee51848c0e8d4e995f382
Tee Parham <tee@neighborland.com>
2014-12-08 09:54:50 -0700

    Add repo exception for jquery-ujs

--- lib/gemdiff/repo_finder.rb
+++ lib/gemdiff/repo_finder.rb
@@ -22,6 +22,7 @@ module Gemdiff
         delayed_job:                 'collectiveidea/delayed_job',
         ffi:                         'ffi/ffi',
         gosu:                        'jlnr/gosu',
+        :"jquery-ujs"             => 'rails/jquery-ujs',
         :"modular-scale"          => 'Team-Sass/modular-scale',
         nokogiri:                    'sparklemotion/nokogiri',
         passenger:                   'phusion/passenger',
