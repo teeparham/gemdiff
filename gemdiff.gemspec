8c7ffb8979e5eba5053661d5c9531444b9208536
Tee Parham <tee@neighborland.com>
2018-02-28 15:45:11 -0700

    Freeze strings

--- gemdiff.gemspec
+++ gemdiff.gemspec
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require "./lib/gemdiff/version"
 
 Gem::Specification.new do |spec|
--- test/bundle_inspector_test.rb
+++ test/bundle_inspector_test.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require "test_helper"
 
 class BundleInspectorTest < MiniTest::Spec
